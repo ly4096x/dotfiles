a71e0875044f2ed0be4da30ec61e8d55dc9f3c24
X <50459973+ly4096x@users.noreply.github.com>
2025-05-29 00:49:19 -0400

    Update keybindings.fish

--- .config/fish/conf.d/keybindings.fish
+++ .config/fish/conf.d/keybindings.fish
@@ -1,3 +1,3 @@
 status is-interactive || exit
 
-bind ctrl-c 'echo; commandline | cat; commandline -f kill-whole-line repaint'
+bind ctrl-c 'echo "^C"; commandline | cat; commandline -f kill-whole-line repaint'
